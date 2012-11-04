41fdb7ca30c645549ba920cc198a54e18b0a52e5
Jan-Jaap Driessen <janjaapdriessen@gmail.com>
2012-11-04 17:30:31 +0100

    target only the td's with anchors

--- scrape.py
+++ scrape.py
@@ -6,5 +6,6 @@ url = 'https://www.nluug.nl/events/nj12/programma.html'
 html = urllib.urlopen(url).read()
 
 root = lxml.html.fromstring(html)
-tds = root.xpath('//table[@id="schedule"]//td')
-print len(tds)
+for entry in root.xpath('//table[@id="schedule"]//td[a]'):
+    anchor = entry.xpath('a')[0]
+    print anchor.get('href')
