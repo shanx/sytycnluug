8c4d2c2f3ebfddc024347f91ae5538a481249e45
Jan-Jaap Driessen <janjaapdriessen@gmail.com>
2012-11-04 18:08:31 +0100

    move to src subdur

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+version = '1.0dev'
+
+setup(
+    name='sytycnluug',
+    version=version,
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'lxml',
+        'Django',
+        ])
