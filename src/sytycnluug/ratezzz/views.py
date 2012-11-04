d546a6b042f223b3b0a039941192749d88d4fd64
Remco Wendt <remco@maykinmedia.nl>
2012-11-04 20:05:26 +0100

    Made ratings db based

--- src/sytycnluug/ratezzz/views.py
+++ src/sytycnluug/ratezzz/views.py
@@ -8,3 +8,13 @@ class RatingCreate(CreateView):
 
 class IndexView(TemplateView):
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+
+        context.update({
+            'talks': Talk.objects.all()
+        })
+
+        return context
+
